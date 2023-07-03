081e519ea43e87fecc9287acce3a9cb232b0b043
skalinowski <slawomir.kalinowski@animex.pl>
2023-07-03 15:10:15 +0200

    .

--- index.js
+++ index.js
@@ -4,36 +4,45 @@ const yamljs = require("yamljs");
 const swaggerJsDoc=yamljs.load("./api.yaml");
 const nodemon = require("nodemon");
 
-const port = process.env.port || 4000;
-const app = express()
+
+const port = process.env.port || 8080;
+const app = express();
+
+app.use(express.json);
 app.use(("/api"), swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
 
-app.get("/string", (req, res) => {
-    res.status(200).send("Działa, zwraca stringa TEST")
-})
 
 const obj={id: 1, name: "Heniek", age: 22}
 
 let users=[
-    {id: 1, name: "Heniek", age: 22},
-    {id: 2, name: "Mirek", age: 23},
-    {id: 3, name: "Gośka", age: 36},
-    {id: 4, name: "Hania", age: 19}
+    {id: 1, name: "Heniek",age: 22},
+    {id: 2, name: "Mirek", age: 25},
+    {id: 3, name: "Gośka", age: 61},
+    {id: 4, name: "Hania", age: 32}
 
 ]
 
+app.get("/string", (req, res) => {
+    res.status(200).send("Działa, zwraca stringa TEST")
+});
+
 app.get("/user", (req, res) => {
     res.status(200).send(obj)
-})
+});
 
 
-app.use("/users", (req, res) => {
+app.get("/users", (req, res) => {
     res.send(users)
-})
+});
+
+app.get("/users/:id", (req, res) => {
+    const obj = users.find((x) => x.id === parseInt(req.params.id)); 
+    res.status(200).send(obj)
+});
+app.get("/create", (req, res) => {
+    users=[req.body,...users]
+    res.send(users)
+});
+
 
 app.listen(port, () => console.log(`The server is running on http://localhost:${port}/api`));
-
-
-
-
-
