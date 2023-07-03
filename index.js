57674f1bf84ff99763ceb9c17a915c73ea4f739c
Womir <slawek.zahun@gmail.com>
2023-07-03 22:18:26 +0200

    POST Grafiki

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const swaggerUI = require("swagger-ui-express");
 const yamljs = require("yamljs");
 const swaggerJsDoc=yamljs.load("./api.yaml");
 const nodemon = require("nodemon");
+const fileUpload = require("express-fileupload");
 
 
 const port = process.env.port || 8080;
 const app = express();
 
 app.use(express.json()); //do POST jsony
+app.use(fileUpload());
 app.use(("/api"), swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
 
 
@@ -39,9 +41,26 @@ app.get("/users/:id", (req, res) => {
     const obj = users.find((x) => x.id === parseInt(req.params.id)); 
     res.status(200).send(obj)
 });
+
 app.post("/create", (req, res) => {
     users=[req.body,...users]
     res.send(users)
+});
+
+
+app.get("/userQuery", (req, res) => {
+    const obj = users.find((x) => x.id === parseInt(req.query.id)); 
+    res.status(200).send(obj)
+});
+
+
+app.post("/upload", (req, res) => {
+    const file= req.files.file 
+    let path = __dirname +"/upload/"+"file_"+Date.now()+".jpg";
+    file.mv(path, (err) =>{
+        res.send("ok")
+    })
+    // res.status(200).send("OK")
 });
 
 
